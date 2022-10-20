320794dc6b18934ecdbcda86396b8b0fe8a9c9a0
yz4004 <yz4004@columbia.edu>
2022-10-20 19:46:36 -0400

    first

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyreactapp2f6b9d31d": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,190 @@
+// ** React Imports
+import { createContext, useEffect, useState, ReactNode } from 'react'
+
+// ** Next Import
+import { useRouter } from 'next/router'
+
+// ** Axios
+import axios from 'axios'
+
+// ** Config
+import authConfig from 'src/configs/auth'
+
+// ** Types
+import { AuthValuesType, RegisterParams, LoginParams, ErrCallbackType, UserDataType } from './types'
+
+import { Auth } from 'aws-amplify';
+
+
+// ** Defaults
+const defaultProvider: AuthValuesType = {
+  user: null,
+  loading: true,
+  setUser: () => null,
+  setLoading: () => Boolean,
+  isInitialized: false,
+  login: () => Promise.resolve(),
+  logout: () => Promise.resolve(),
+  setIsInitialized: () => Boolean,
+  register: () => Promise.resolve()
+}
+
+const AuthContext = createContext(defaultProvider)
+
+type Props = {
+  children: ReactNode
+}
+
+const AuthProvider = ({ children }: Props) => {
+  // ** States
+  const [user, setUser] = useState<UserDataType | null>(defaultProvider.user)
+  const [loading, setLoading] = useState<boolean>(defaultProvider.loading)
+  const [isInitialized, setIsInitialized] = useState<boolean>(defaultProvider.isInitialized)
+
+  // ** Hooks
+  const router = useRouter()
+
+  useEffect(() => {
+    const initAuth = async (): Promise<void> => {
+      setIsInitialized(true)
+      const storedToken = window.localStorage.getItem(authConfig.storageTokenKeyName)!
+      if (storedToken) {
+        setLoading(true)
+        await axios
+          .get(authConfig.meEndpoint, {
+            headers: {
+              Authorization: storedToken
+            }
+          })
+          .then(async response => {
+            setLoading(false)
+            setUser({ ...response.data.userData })
+          })
+          .catch(() => {
+            localStorage.removeItem('userData')
+            localStorage.removeItem('refreshToken')
+            localStorage.removeItem('accessToken')
+            setUser(null)
+            setLoading(false)
+          })
+      } else {
+        setLoading(false)
+      }
+    }
+    initAuth()
+  }, [])
+
+  const handleLogin = async (params: LoginParams, errorCallback?: ErrCallbackType) => {
+    axios
+      .post(authConfig.loginEndpoint, params)
+      .then(async res => {
+        window.localStorage.setItem(authConfig.storageTokenKeyName, res.data.accessToken)
+      })
+      .then(() => {
+        axios
+          .get(authConfig.meEndpoint, {
+            headers: {
+              Authorization: window.localStorage.getItem(authConfig.storageTokenKeyName)!
+            }
+          })
+          .then(async response => {
+            const returnUrl = router.query.returnUrl
+
+            setUser({ ...response.data.userData })
+            await window.localStorage.setItem('userData', JSON.stringify(response.data.userData))
+
+            const redirectURL = returnUrl && returnUrl !== '/' ? returnUrl : '/'
+
+            router.replace(redirectURL as string)
+          })
+      })
+      .catch(err => {
+        if (errorCallback) errorCallback(err)
+      })
+
+    // if (!email.value) {
+    //   setEmail({ value: '', isValid: false });
+    //   return;
+    // }
+
+    // if (!password.value) {
+    //   setPassword({ value: '', isValid: false });
+    //   return;
+    // }
+
+    // setIsLoading(true);
+
+    // try {
+    //   await Auth.signIn({ username: params.email, password: params.password });
+
+    //   const returnUrl = router.query.returnUrl
+    //   const redirectURL = returnUrl && returnUrl !== '/' ? returnUrl : '/'
+    //   //router.push('/');
+    //   router.replace(redirectURL as string)
+
+    // } catch (error) {
+    //   console.error(error);
+    // }
+
+    // setIsLoading(false);
+  }
+
+  const handleLogout = () => {
+    setUser(null)
+    setIsInitialized(false)
+    window.localStorage.removeItem('userData')
+    window.localStorage.removeItem(authConfig.storageTokenKeyName)
+    router.push('/login')
+  }
+
+  const handleRegister = async (params: RegisterParams, errorCallback?: ErrCallbackType) => {
+    axios
+      .post(authConfig.registerEndpoint, params)
+      .then(res => {
+        if (res.data.error) {
+          if (errorCallback) errorCallback(res.data.error)
+        } else {
+          handleLogin({ email: params.email, password: params.password })
+        }
+      })
+      .catch((err: { [key: string]: string }) => (errorCallback ? errorCallback(err) : null))
+
+
+
+      // try {
+      //   await Auth.signUp({
+      //     username: params.email,
+      //     password: params.password,
+      //     attributes: {
+      //       email: params.email,
+      //      // name: `${firstName.value} ${lastName.value}`,
+      //      // name: `${params.username} ${params.email}`,
+      //     },
+      //   });
+  
+      //   if (typeof window !== 'undefined') {
+      //     localStorage.setItem('pending_verification_email', params.email);
+      //   }
+  
+      //   //router.push('/confirm');
+      // } catch (error) {
+      //   console.error(error);
+      // }
+  }
+
+  const values = {
+    user,
+    loading,
+    setUser,
+    setLoading,
+    isInitialized,
+    setIsInitialized,
+    login: handleLogin,
+    logout: handleLogout,
+    register: handleRegister
+  }
+
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>
+}
+
+export { AuthContext, AuthProvider }
